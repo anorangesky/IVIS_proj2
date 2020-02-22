cabe8cf7090c8fcf345a2c438c9d952bea6edf89
agnespet <agnespet@kth.se>
2020-02-22 17:27:13 +0100

    added csv data

---
+++ js/ridgeline.js
@@ -0,0 +1,92 @@
+// set the dimensions and margins of the graph
+var margin = {top: 60, right: 30, bottom: 20, left:110},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#ridgeline")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+/*
+  Read data
+    Mocked data from https://www.generatedata.com/#t1 
+    A total of 1000 rows. 
+    200 rows from each wave.
+
+    - "Wave" in years where "2": 90-94; "3": 95-98; "4": 99-04; "5": 05-09; "6": 10-14
+    - "Country" takes a random country 
+    - "Organization" takes a random organization out of 8 possible 
+    - "Value" is how much confidence in the organisation. "1": Great deal; "2": Quite a lot; "3": not very much; "4": not at all
+*/
+d3.csv("myData.csv", function(data) {
+
+  // Get the different categories and count them
+  var countries = data["Country"]
+  var n = countries.length
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([-10, 140])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Create a Y scale for densities
+  var y = d3.scaleLinear()
+    .domain([0, 0.4])
+    .range([ height, 0]);
+
+  // Create the Y axis for names
+  var yName = d3.scaleBand()
+    .domain(countries)
+    .range([0, height])
+    .paddingInner(1)
+  svg.append("g")
+    .call(d3.axisLeft(yName));
+
+  // Compute kernel density estimation for each column:
+  var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+  var allDensity = []
+  for (i = 0; i < n; i++) {
+      key = countries[i]
+      density = kde( data.map(function(d){  return d[key]; }) )
+      allDensity.push({key: key, density: density})
+  }
+
+  // Add areas
+  svg.selectAll("areas")
+    .data(allDensity)
+    .enter()
+    .append("path")
+      .attr("transform", function(d){return("translate(0," + (yName(d.key)-height) +")" )})
+      .datum(function(d){return(d.density)})
+      .attr("fill", "#69b3a2")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 1)
+      .attr("d",  d3.line()
+          .curve(d3.curveBasis)
+          .x(function(d) { return x(d[0]); })
+          .y(function(d) { return y(d[1]); })
+      )
+
+})
+
+// This is what I need to compute kernel density estimation
+function kernelDensityEstimator(kernel, X) {
+  return function(V) {
+    return X.map(function(x) {
+      return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+    });
+  };
+}
+function kernelEpanechnikov(k) {
+  return function(v) {
+    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+  };
+}
